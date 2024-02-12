5dfd97f3f634447a4651f0242134924dd1bcfd94
trisuhartono <102866548+trisuhartono@users.noreply.github.com>
2024-02-12 23:50:59 +0700

    Update Cantik.py

--- Cantik.py
+++ Cantik.py
@@ -25,7 +25,7 @@ if buffer is None:
      st.text("silakan coba lagi dan input citra X-Ray kembali.")
 
 else:
-    img = image.load_img(temp_file.name, target_size=(500, 500),color_mode='grayscale')
+    img = image.load_img(temp_file.name, target_size=(50, 50),color_mode='grayscale')
 
   
     image = Image.open(temp)
