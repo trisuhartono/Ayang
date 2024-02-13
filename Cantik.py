f25f8b65df058b02a50b8bcf76507d1efcbdee1a
trisuhartono <102866548+trisuhartono@users.noreply.github.com>
2024-02-13 15:51:16 +0700

    Update Cantik.py

--- Cantik.py
+++ Cantik.py
@@ -33,18 +33,11 @@ if selected == 'BERANDA':
         st.write("")
 
     st.write('<center><h1>SELAMAT HARI VALENTINE AYANGKUUU❤❤❤</h1></center>', unsafe_allow_html=True)
-    #st.write('''<center><h3>APLIKASI INI DIRANCANG SEBAGAI TUGAS AKHIR di SEKOLAH TINGGI ILMU KESEHATAN SEMARANG</h3></center>''', unsafe_allow_html=True)
-  
-    #st.write('')
-    #st.write('')
-    #st.write('''<center><h6>Tahun Akademik 2022/2023</h6></center>''', unsafe_allow_html=True)
-    #st.write('''<center><h6>Oleh : TRI SUHARTONO</h6></center>''', unsafe_allow_html=True)
-
-#st.set_page_config(page_title="trisuhartono klasifikasi pneumonia")
+    
 
     
 if selected == 'UNDANGAN':
-    st.write('undungan nih buat ayaang❤❤')
+    st.write('undangan nih buat ayaangkuu❤❤')
     st.write ('') 
     st.write('Kepada Yang Tercantik: Ayangkuuu Dek Anis Muzkiyah')
     st.write('Hal: kartu Ucapan')
@@ -53,19 +46,19 @@ if selected == 'UNDANGAN':
     st.write('Dengan penuh Cinta') 
     st.write('Dengan datangnya kartu ucapan ini, yang bertanda tangan dibawah ini') 
     st.write('Nama: Tri suhartono') 
-    st.write('Jabatan : Calon suami Anis Muzkiyah') 
+    st.write('Jabatan : suami Anis Muzkiyah') 
     st.write('Masa Jabatan : SELAMANYA') 
     st.write('') 
     st.write('Dengan ini mengucapkan selamat HARI VALENTINE 2024.') 
-    st.write('Demikian Kartu ucapan ini saya buat dengan cinta seluah alam raya terkhusus bidadariku yang cantiiik, yang maniiisss, yang baiikkk, dan tak ada duanya') 
+    st.write('Demikian Kartu ucapan ini mamas buat dengan cinta seluas alam raya terkhusus bidadariku yang cantiiik, yang maniiisss, yang baiikkk, dan tak ada duanya') 
     st.write('') 
     st.write ('') 
     st.write ('Terdanda') 
     st.write('Mamas Ganteng') 
 if selected == 'UPLOAD DONG':
-    st.write('yaaa, halaman terkhir nih ay🤭')
+    st.write('halaman sebelum terakhir..')
     st.write ('') 
-    temp = st.file_uploader("Uplod foto cantiknya dooong ayaangkuu 🥰🥰🥰")
+    temp = st.file_uploader("Upload foto cantiknya dooong ayaangkuu 🥰🥰🥰")
     buffer = temp
     temp_file = NamedTemporaryFile(delete=False)
     if buffer:
@@ -83,7 +76,7 @@ if selected == 'UPLOAD DONG':
         st.write('<center><h3>Semangat kuliahe nggih ayaangkuuu❤❤❤, Mugi diparingi lancar sedoyo nggih cantiiiik❤❤❤</h3></center>', unsafe_allow_html=True) 
       
 if selected == 'WITH MY LOVE':
-    st.write('Haloo ayaangkuu 🥰🥰🥰')
+    st.write('Haloo ayaangkuu, halaman terakhir niih🥰🥰🥰')
     
     col1, col2 = st.columns([1, 1])
     with col1:
