f373fc878cd7aa22cd508dbd2891154a0d375b42
trisuhartono <102866548+trisuhartono@users.noreply.github.com>
2024-02-13 15:36:33 +0700

    Update Cantik.py

--- Cantik.py
+++ Cantik.py
@@ -123,7 +123,7 @@ if selected == 'WITH MY LOVE':
         st.image(resized_image, caption='', use_column_width=True)
     
     with col2:
-        st.image4(resized_image4, caption='', use_column_width=True)
+        st.image4(resized_image, caption='', use_column_width=True)
     
 
 
